86125759c29e5d13db41f041df948b6ea0a73b33
Till Hein <tillhein@web.de>
2019-02-27 12:26:53 +0100

    changes for local testing

--- BreadthFirstGatherer.py
+++ BreadthFirstGatherer.py
@@ -2,117 +2,90 @@ from AbsFrontierStrategy import AbsFrontierStrategy
 import urllib
 import datetime
 class BreadthFirstGatherer(AbsFrontierStrategy):
+    def queue(self, link):
+        try:
+            link = self.__canonicalizeLinks(link)
+        except Exception as e:
+            print('Frontier: Error canonicalizing link' + str(e))
+            return True
+        try:
+            hashUrl = self.__parseUrl(link)
+        except Exception:
+            print('Frontier: Error stripping URL')
+            return True
+        try:
+            if str(hashUrl) not in self.visited:
+                self.frontier.insert(0, link)
+                self.visited[hashUrl] = 1
+                #print('Frontier: ' + link + ' added')
+        except Exception:
+            print('Frontier: Error ')
+        return True
+
+    def dequeue(self):
+        if self.max > 0:
+            self.max = self.max - 1
+            return self.frontier.pop()
+        else:
+            print("Frontier: Reached Crawl Limit")
+            return None
+
+    def size(self):
+        return len(self.frontier)
+
+    def __parseUrl(self, url):
+        parsedUrl = urllib.parse.urlparse(url)
+        return parsedUrl.netloc + parsedUrl.path
+
+    def __init__(self):
+        self.frontier = list()
+        #{link: layer}
+        self.visited = {}
+        self.layer = 0
+        self.count = 0
+        self.max = 20
 
 
-	#link[link, source]
-	def queue(self, link):
-		#Edgecase first entries from seed list
-		if type(link) is not list:
-			link = self.__canonicalizeLinks(link)
-			self.frontier.insert(0,link)
-			print(link + ' seeded')
-			self.visited[self.__parseUrl(link)] = 0
-			self.lenght = self.lenght + 1
-		elif type(link) is list:
-			try:
-				link[0] = self.__canonicalizeLinks(link[0])
-				link[1] = self.__canonicalizeLinks(link[1])
-			except Exception:
-				print('GATHERER: Error canonicalizing')
-				return True
-			try:
-				hashUrl = self.__parseUrl(link[0])
-			except Exception:
-				print('GATHERER Error in parsing')
-				return True
-			
-			try:
-				if str(hashUrl) not in self.visited:
-					self.frontier.insert(0,link[0])
-					self.visited[hashUrl] = 0
-					self.lenght = self.lenght + 1
-			except Exception:
-				print('GATHERER: Error in adding to frontier')
-				return True
+    def __canonicalizeLinks(self, link):
+    # Remove Port
+    # Add trailing slash (root and guessed directorys)
+    # remove Fragments and options
+    # resolve Path (http://cs.india.edu/a/./../b/ -> http://cs.india.edu/b/
+    # Remove default filenames (index.html)
+    # decode needlessly encoded characters like '%7' -> '~'
+    # encode forbidden characters like ' ' -> '%20'
+        link = link.replace('///', '//')
+        parsedLink = urllib.parse.urlparse(link)
+        scheme = parsedLink.scheme
+        netloc = parsedLink.hostname
+        if ':' in netloc:
+            netloc = netloc.split(':')[0]
+        netloc = 'localhost/crawler/' + netloc
+        path = parsedLink.path
+        steps = [str(f) for f in path.split('/') if f.strip()]
+        steplen = len(steps)
+        #only netloc -> create index.html
+        if (steplen == 0):
+            if path.endswith('/'):
+                path = path[:-1]
+            path = path + '/' + 'index.html'
+        elif '.' not in steps[steplen-1]:
+            if path.endswith('/'):
+                path = path[:-1]
+            path = path + '.html'
 
-		else:
-			print('GATHER: WRONG TYPE')
-			return True
-
-
-		#	if 'portal.dnb.de' in hashUrl:
-		#		print ('Add ' + hashUrl + 'to Frontier')
-		#	try:
-		#		if self.visited[self.__parseUrl(link[1])] < 1:
-		#			#print('add to Frontier: ' + link[0])
-		#			self.frontier.insert(0,link[0])
-		#			self.visited[hashUrl] = self.visited[self.__parseUrl(link[1])] + 1
-		#			
-		#			if 'portal.dnb.de/opec.htm' in hashUrl:
-		#				print ('Add ' + hashUrl + 'to Frontier')
-		#			self.lenght = self.lenght + 1
-		#	
-		#	except Exception as err:
-		#		print('Key Error: ' + self.__parseUrl(link[1]))
-				
-			#	for key, value in self.viseted.iteritems():
-			#		print (key, value)
-
-
-
-
-
-
-		print(str(datetime.datetime.now()) + ': GATHER LEN: ' + str(self.lenght))
-		return True
-
-	def dequeue(self):
-		if self.lenght > 0:
-			self.count = self.count + 1
-			self.lenght = self.lenght - 1
-			return self.frontier.pop()
-		else:
-			print("Frontier is empty")
-			return None
-
-	def size(self):
-		return len(self.frontier)
-
-	def __parseUrl(self, url):
-		parsedUrl = urllib.parse.urlparse(url)
-		return parsedUrl.netloc + parsedUrl.path
-
-	def __init__(self):
-		self.frontier = list()
-		#{link: layer}
-		self.visited = {}
-		self.layer = 0
-		self.count = 0
-		self.lenght = 0
-
-
-	def __canonicalizeLinks(self, link):
-	# Remove Port
-	# Add trailing slash (root and guessed directorys)
-	# remove Fragments and options
-	# resolve Path (http://cs.india.edu/a/./../b/ -> http://cs.india.edu/b/
-	# Remove default filenames (index.html)
-	# decode needlessly encoded characters like '%7' -> '~'
-	# encode forbidden characters like ' ' -> '%20'
-		
-
-		link = link.replace('///', '//')
-		parsedLink = urllib.parse.urlparse(link)
-		scheme = parsedLink.scheme
-		netloc = parsedLink.hostname
-		if ':' in netloc:
-			netloc = netloc.split(':')[0]
-		path = parsedLink.path
-		qs = parsedLink.query
-		#scheme, netloc, path, qs, anchor = urllib.parse.urlsplit(link)
-		path = urllib.parse.quote(path, '/%')
-		qs = urllib.parse.quote_plus(qs, ':&=')
-		anchor = ''
-		if scheme == 'http':
-			scheme = 'https'
-		return urllib.parse.urlunsplit((scheme, netloc, path, qs, anchor))
+        """
+        if (len(path) == 0):
+            path = 'index.html'
+        elif (path.endswith('/')):
+            path = path[:-1] + '.html'
+        elif not ((path.endswith('.html')) or (path.endswith('.htm'))):
+            path = path + '.html'
+        """            
+        qs = parsedLink.query
+        #scheme, netloc, path, qs, anchor = urllib.parse.urlsplit(link)
+        path = urllib.parse.quote(path, '/%')
+        qs = urllib.parse.quote_plus(qs, ':&=')
+        anchor = ''
+        scheme = 'http'
+        return urllib.parse.urlunsplit((scheme, netloc, path, qs, anchor))
