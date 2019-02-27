8179aaa330b8a4b3e4840540444190fc29534e2a
Till Hein <tillhein@web.de>
2019-02-27 12:14:27 +0100

    removed response status check

--- DownloadState.py
+++ DownloadState.py
@@ -7,29 +7,22 @@ import requests
 #from Response import Response
 
 #def signal_handler(signum, frame):
-#	with open( 'timeout.log' , 'a' )  as t:
-#		t.write('TIMEOUT: ' + str(datetime.datetime.now()))
-#	raise Exception ('timed out')
+#   with open( 'timeout.log' , 'a' )  as t:
+#       t.write('TIMEOUT: ' + str(datetime.datetime.now()))
+#   raise Exception ('timed out')
 
 class DownloadState(AbsDownloadState):
 
 
-	#@return Respnose Contains Downloaded Site
-	#@return None An Error Occurred (Add Log here)
-	def handle(self, task):
-		session = requests.Session()
-		#signal.signal(signal.SIGALRM, signal_handler)
-		#signal.alarm(10)
-		try:
-			response = session.send(task, timeout = 10.0)
-		#TODO: Add Logfile entries for Exception hits
-		except Exception as e:
-			print('Error in DownloadState.py :' + task.url)
-			return None
-		else:
-			if response.ok:
-				print('OK')
-				return response
-			else:
-				
-				return None
+    #@return Respnose Contains Downloaded Site
+    def handle(self, task):
+        session = requests.Session()
+        #signal.signal(signal.SIGALRM, signal_handler)
+        #signal.alarm(10)
+        try:
+            response = session.send(task, timeout = 10.0)
+        #TODO: Add Logfile entries for Exception hits
+        except Exception as e:
+            print('Error in DownloadState.py: ' + task.url)
+            return None
+        return response
