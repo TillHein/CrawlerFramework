a4c88620a3ec46ec0815f33b0fbc328b8307e438
Till Hein <tillhein@web.de>
2019-02-27 12:46:09 +0100

    print exception information

--- CrawlerMediator.py
+++ CrawlerMediator.py
@@ -2,56 +2,56 @@ import signal
 import datetime
 
 def signal_handler(signum, frame):
-	with open( 'timeout.log' , 'a' )  as t:
-		t.write('TIMEOUT: ' + str(datetime.datetime.now()))
-	raise Exception ('timed out')
+    with open( 'timeout.log' , 'a' )  as t:
+        t.write('TIMEOUT: ' + str(datetime.datetime.now()))
+    raise Exception ('timed out')
 
 class CrawlerMediator:
 
-	"""Call this function to start the crawl process"""
-	def crawl(self):
-		nextUrl = self._frontier.dequeue()
-		while(nextUrl != None):
-			try:
-				signal.signal(signal.SIGALRM, signal_handler)
-				signal.alarm(10)
-				self._download.download(nextUrl)
-			except Exception:
-				print('MEDIATOR: Unknown error in download')
-			try:
-				nextUrl = self._frontier.dequeue()
-			except Exception:
-				print('MEDIATPOR: Dequeue Error')
-		return
+    """Call this function to start the crawl process"""
+    def crawl(self):
+        nextUrl = self._frontier.dequeue()
+        while(nextUrl != None):
+            try:
+                signal.signal(signal.SIGALRM, signal_handler)
+                signal.alarm(10)
+                self._download.download(nextUrl)
+            except Exception:
+                print('MEDIATOR: Unknown error in download')
+            try:
+                nextUrl = self._frontier.dequeue()
+            except Exception as e:
+                print('MEDIATPOR: Dequeue Error' + str(e))
+        return
 
-	"""Function used by DownloadStateContext to Store new URLs"""
-	def queue(self, url):
-		try:
-			self._frontier.queue(url)
-		except Exception:
-			print('MEDIATOR: Unknown error in queue')
-		return
+    """Function used by DownloadStateContext to Store new URLs"""
+    def queue(self, url):
+        try:
+            self._frontier.queue(url)
+        except Exception as e:
+            print('MEDIATOR: Unknown error in queue: ' + str(e))
+        return
 
-	"""Function used by DownloadStateContext to Store Crawled Information
-	 Implementation unclear"""
-	def store(self, data):
-		try:
-			self._store.store(data)
-		except Exception:
-			print('MEDIATOR: Unknown error in queue')
-		return
-	
-	"""set Functions to connect the different contexts"""
-	def setFrontierContext(self, frontier):
-		self._frontier = frontier
-		return
+    """Function used by DownloadStateContext to Store Crawled Information
+     Implementation unclear"""
+    def store(self, data):
+        try:
+            self._store.store(data)
+        except Exception as e:
+            print('MEDIATOR: Unknown error in store' + str(e))
+        return
+    
+    """set Functions to connect the different contexts"""
+    def setFrontierContext(self, frontier):
+        self._frontier = frontier
+        return
 
-	def setStoreContext(self, store):
-		self._store = store
-		return
-	
-	def setDownloadStateContext(self, download):
-		self._download = download
-		return
+    def setStoreContext(self, store):
+        self._store = store
+        return
+    
+    def setDownloadStateContext(self, download):
+        self._download = download
+        return
 
 
